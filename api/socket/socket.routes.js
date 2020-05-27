21c49a2ce0f38759620f17afcbe2917163c5de6f
aviad777 <aviadhaimjan@gmail.com>
2020-05-27 22:00:12 +0300

    socket done

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -3,25 +3,18 @@ module.exports = connectSockets
 
 function connectSockets(io) {
     io.on('connection', socket => {
-        console.log('a user is connected');
         socket.on('disconnect', () => {
-            console.log('user disconnected');
         })
 
-        socket.on('board updated', boardId => {
-            console.log('update event on socket factory', boardId)
-            // io.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            socket.broadcast.to(socket.currBoard).emit('board updated', boardId)
+        socket.on('board updated', () => {
+            socket.broadcast.to(socket.currBoard).emit('board updated')
         })
-        socket.on('boardLoad', boardId => {
+        socket.on('open board socket', boardId => {
             if (socket.currBoard) {
                 socket.leave(socket.currBoard)
             }
             socket.join(boardId)
             socket.currBoard = boardId;
-            console.log('new board loaded', boardId);
-
         })
     })
 }
