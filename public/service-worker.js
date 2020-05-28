7228518502c01898a74956ab11df9708525dd5db
orlevy13 <orlevy13d@gmail.com>
2020-05-28 14:25:37 +0300

    url fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47e0e68768a0643b16533206869f185d.js"
+  "/precache-manifest.b5fc35045b94f3e64b831dd50361310c.js"
 );
 
 self.addEventListener('message', (event) => {
--- server.js
+++ server.js
@@ -20,29 +20,16 @@ app.use(session({
     cookie: { secure: false }
 }))
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.resolve(__dirname, 'public')));
-// } else {
-//     const corsOptions = {
-//         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-//         credentials: true
-//     };
-//     app.use(cors(corsOptions));
-// }
-
-
-const authRoutes = require('./api/auth/auth.routes')
-const userRoutes = require('./api/user/user.routes')
-const boardRoutes = require('./api/board/board.routes')
-const connectSockets = require('./api/socket/socket.routes')
-
-
 // routes
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/board', boardRoutes)
 connectSockets(io)
 
+const authRoutes = require('./api/auth/auth.routes')
+const userRoutes = require('./api/user/user.routes')
+const boardRoutes = require('./api/board/board.routes')
+const connectSockets = require('./api/socket/socket.routes')
 
 
 const logger = require('./services/logger.service')
